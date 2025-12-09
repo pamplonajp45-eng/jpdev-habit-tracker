1e0ff5abda0ed651fb23990fb3ef3544e3170a0f
JPDEV <pamplonajp.45@gmail.com>
2025-12-09 23:18:45 +0800

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/jpdev-habit-tracker/' 
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'HABBITLOGO.png', 'HABBITLOGO.png'],
+      manifest: {
+        name: 'Habit Tracker',
+        short_name: 'HBT',
+        description: 'Track your habits easily!',
+        theme_color: '#4f46e5',
+        background_color: '#0a0a15',
+        display: 'standalone',
+        start_url: '/jpdev-habit-tracker/',
+        icons: [
+          {
+            src: 'HABBITLOGO.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'HABBITLOGO.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
+  base: '/jpdev-habit-tracker/'
 })
